089482357d1a703fc5df8c2275778a345105936c
akashmngkmit <akashnandan@gkmit.co>
2025-11-14 15:15:24 +0530

    feat(ui): add herosection basic strcuture

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
-import { Navbar } from '@/components/navbar'
-import React from 'react'
+import { HeroSection } from '@/components/HeroSection';
+import { Navbar } from '@/components/navbar';
+import React from 'react';
 
 export const LandingPage = () => {
   return (
     <>
-        <Navbar />
+      <Navbar />
+      <HeroSection />
     </>
-  )
-}
+  );
+};
