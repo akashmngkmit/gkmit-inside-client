0a2ca07a8f8fa0a86237031402c99e5a0d8eeab3
akashmngkmit <akashnandan@gkmit.co>
2025-11-14 16:02:32 +0530

    feat(ui): refine the design with code

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -37,12 +37,10 @@ export function HeroSection() {
         ref={vantaRef} 
         className="absolute top-0 left-0 w-full h-full z-0"
       />
-      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen p-4">
-        <div className="bg-black/50 backdrop-blur-sm w-full max-w-6xl p-6 md:p-12 flex flex-col items-center justify-center gap-6 md:gap-8 border-2 border-white border-dotted rounded-2xl text-center">
-          
-          <h1 className="text-6xl md:text-7xl font-bold text-white">Welcome to GKMIT INSIDE</h1>
-          
-          <p className="text-lg md:text-xl max-w-3xl text-gray-200">
+      <div className="relative z-10 flex flex-col items-center md:items-start h-auto justify-center min-h-screen p-4">
+        <div className="bg-black/50 backdrop-blur-sm w-full max-w-3xl h-[550px] md:ml-60 p-6 md:p-12 flex flex-col items-center justify-center gap-6 md:gap-10 border-2 border-white border-dotted rounded-2xl text-center">
+          <h1 className="text-6xl md:text-7xl font-bold text-white md:tracking-wider leading-tight">Welcome to GKMIT INSIDE</h1>
+          <p className="text-lg md:text-xl max-w-3xl text-gray-200 md:leading-loose">
             Your internal hub for company news, team updates, and connecting with
             colleagues. Share your projects, celebrate successes, and stay in the
             loop with everything happening at GKMIT.
