84c373c59341be6ec7648a529a28d9b550ec3e91
akashmngkmit <akashnandan@gkmit.co>
2025-11-16 14:07:23 +0530

    feat: add create post component to feed page

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";import { CreatePost } from '../components/CreatePost'; 
 
 export const FeedPage = () => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Main Feed</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p className="text-gray-600">
-          The `CreatePost` and `PostCard` components will go here.
-        </p>
-      </CardContent>
-    </Card>
+    <div className="flex flex-col gap-6">
+      <CreatePost />
+      <Card>
+        <CardHeader>
+          <CardTitle>Feed</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-gray-600">
+            The `PostCard` components will go here.
+          </p>
+        </CardContent>
+      </Card>
+
+    </div>
   );
 };
