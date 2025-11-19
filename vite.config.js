07262bfd0f4497486646c6a98129e6bf9f890a61
akashmngkmit <akashnandan@gkmit.co>
2025-11-19 14:58:58 +0530

    feat: integrate pwa

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -11,7 +12,36 @@ const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Gkmit Inside',
+        short_name: 'Inside',
+        description: 'Gkmit Inside : Internal Know-hub',
+        theme_color: '#FFFFFF',
+        start_url: '/',
+        scope: '/',
+        display: 'fullscreen',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
